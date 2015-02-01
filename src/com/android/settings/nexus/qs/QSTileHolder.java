fd2f6966f853921e9916a150094cc70894f4267e
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-02-01 07:23:50 +0000

    QS: add Sync tile (2/2)

--- src/com/android/settings/nexus/qs/QSTileHolder.java
+++ src/com/android/settings/nexus/qs/QSTileHolder.java
@@ -124,6 +124,10 @@ public class QSTileHolder {
                 resourceName = "ic_qs_screen_timeout_vector";
                 stringId = R.string.qs_tile_screen_timeout;
                 break;
+            case QSConstants.TILE_SYNC:
+                resourceName = "ic_qs_sync_on";
+                stringId = R.string.qs_tile_sync;
+                break;
             case QSConstants.TILE_NOTIFICATIONS:
                 resourceName = "ic_qs_ringer_audible";
                 stringId = R.string.qs_notifications_tile;
