c2d8a0fcff821f2cfffc9ab3daec653b1d1e36e2
Griffin Millender <griffinn.millender@gmail.com>
2015-05-31 16:25:53 -0500

    Settings: Slim Recents app screen (2/2)
    PS10 :
    
    - use slimseekbar for recent panel size (60% - 160% in steps of 5%)
    - fix maxapps to have a minimum value of 5
    
    Original patch can be found here:
    https://github.com/SlimRoms/packages_apps_Settings/commit/180579b879507ee16e4ed3277ae92abaa865b6a5
    
    Euphoria-OS amendments
    -disable by default
    -use existing SeekBar, fix minimum value and add interval
    
    PureNexus changes:
    * made general category for options that work on both aosp/slim
    * updated color options based off of @dwitherell colorpicker

--- src/com/android/settings/nexus/RecentAppSettings.java
+++ src/com/android/settings/nexus/RecentAppSettings.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2015 The Pure Nexus Project
+ * Copyright (C) 2015 SlimRoms Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,8 +14,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.android.settings.nexus;
 
+import android.app.ActivityManager;
+import android.content.Context;
 import android.content.ContentResolver;
 import android.content.res.Resources;
 import android.database.ContentObserver;
@@ -24,21 +28,51 @@ import android.os.UserHandle;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceCategory;
 import android.preference.PreferenceScreen;
 import android.preference.SwitchPreference;
 import android.provider.Settings;
+import android.provider.Settings.SettingNotFoundException;
+import android.view.Gravity;
 
+import com.android.settings.nexus.SeekBarPreference;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.margaritov.preference.colorpicker.ColorPickerPreference;
 
 public class RecentAppSettings extends SettingsPreferenceFragment
         implements OnPreferenceChangeListener {
 
+    private static final String SHOW_RECENTS_SEARCHBAR = "recents_search_bar";
     private static final String SHOW_CLEAR_ALL_RECENTS = "show_clear_all_recents";
     private static final String RECENTS_CLEAR_ALL_LOCATION = "recents_clear_all_location";
+    private static final String RECENTS_DISMISS_ALL = "recents_clear_all_dismiss_all";
+    private static final String USE_SLIM_RECENTS = "use_slim_recents";
+    private static final String RECENTS_MAX_APPS = "recents_max_apps";
+    private static final String RECENT_PANEL_SCALE = "recent_panel_scale_factor";
+    private static final String RECENT_PANEL_EXPANDED_MODE = "recent_panel_expanded_mode";
+    private static final String RECENT_PANEL_LEFTY_MODE = "recent_panel_lefty_mode";
+    private static final String RECENT_PANEL_BG_COLOR = "recent_panel_bg_color";
+	private static final String RECENT_CARD_BG_COLOR = "recent_card_bg_color";
+    private static final String RECENT_CARD_TEXT_COLOR = "recent_card_text_color";
 
+	private SwitchPreference mRecentsSearchBar;
     private SwitchPreference mRecentsClearAll;
     private ListPreference mRecentsClearAllLocation;
+    private SwitchPreference mRecentsDismissAll;
+    private SwitchPreference mUseSlimRecents;
+    private SeekBarPreference mMaxApps;
+    private SeekBarPreference mRecentPanelScale;
+    private ListPreference mRecentPanelExpandedMode;
+    private SwitchPreference mRecentPanelLeftyMode;
+    private ColorPickerPreference mRecentPanelBgColor;
+    private ColorPickerPreference mRecentCardBgColor;
+    private ColorPickerPreference mRecentCardTextColor;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -47,65 +81,147 @@ public class RecentAppSettings extends SettingsPreferenceFragment
         ContentResolver resolver = getActivity().getContentResolver();
         PreferenceScreen prefSet = getPreferenceScreen();
 
+        mRecentsSearchBar = (SwitchPreference) prefSet.findPreference(SHOW_RECENTS_SEARCHBAR);
         mRecentsClearAll = (SwitchPreference) prefSet.findPreference(SHOW_CLEAR_ALL_RECENTS);
-        mRecentsClearAll.setChecked(Settings.System.getIntForUser(resolver,
-            Settings.System.SHOW_CLEAR_ALL_RECENTS, 0, UserHandle.USER_CURRENT) == 1);
-        mRecentsClearAll.setOnPreferenceChangeListener(this);
+        mRecentsDismissAll = (SwitchPreference) prefSet.findPreference(RECENTS_DISMISS_ALL);
 
         mRecentsClearAllLocation = (ListPreference) prefSet.findPreference(RECENTS_CLEAR_ALL_LOCATION);
         int location = Settings.System.getIntForUser(resolver,
                 Settings.System.RECENTS_CLEAR_ALL_LOCATION, 3, UserHandle.USER_CURRENT);
         mRecentsClearAllLocation.setValue(String.valueOf(location));
+        mRecentsClearAllLocation.setSummary(mRecentsClearAllLocation.getEntry());
         mRecentsClearAllLocation.setOnPreferenceChangeListener(this);
-        updateRecentsLocation(location);
+
+        mUseSlimRecents = (SwitchPreference) prefSet.findPreference(USE_SLIM_RECENTS);
+        mUseSlimRecents.setChecked(Settings.System.getInt(resolver,
+                Settings.System.USE_SLIM_RECENTS, 0) == 1);
+        mUseSlimRecents.setOnPreferenceChangeListener(this);
+
+        mMaxApps = (SeekBarPreference) findPreference(RECENTS_MAX_APPS);
+        int maxApps = Settings.System.getInt(resolver,
+                Settings.System.RECENTS_MAX_APPS, ActivityManager.getMaxRecentTasksStatic());
+        mMaxApps.setValue(maxApps);
+        mMaxApps.setOnPreferenceChangeListener(this);
+
+        mRecentPanelScale = (SeekBarPreference) findPreference(RECENT_PANEL_SCALE);
+        int recentPanelScale = Settings.System.getInt(resolver,
+                Settings.System.RECENT_PANEL_SCALE_FACTOR, 100);
+        mRecentPanelScale.setValue(recentPanelScale);
+        mRecentPanelScale.setOnPreferenceChangeListener(this);
+
+        mRecentPanelExpandedMode = (ListPreference) prefSet.findPreference(RECENT_PANEL_EXPANDED_MODE);
+        int recentPanelExpandedMode = Settings.System.getIntForUser(resolver,
+                Settings.System.RECENT_PANEL_EXPANDED_MODE, 0, UserHandle.USER_CURRENT);
+        mRecentPanelExpandedMode.setValue(String.valueOf(recentPanelExpandedMode));
+        mRecentPanelExpandedMode.setSummary(mRecentPanelExpandedMode.getEntry());
+        mRecentPanelExpandedMode.setOnPreferenceChangeListener(this);
+
+        mRecentPanelLeftyMode = (SwitchPreference) prefSet.findPreference(RECENT_PANEL_LEFTY_MODE);
+        mRecentPanelLeftyMode.setChecked(Settings.System.getInt(resolver,
+                Settings.System.RECENT_PANEL_GRAVITY, Gravity.RIGHT) == Gravity.LEFT);
+        mRecentPanelLeftyMode.setOnPreferenceChangeListener(this);
+
+        mRecentPanelBgColor = (ColorPickerPreference) findPreference(RECENT_PANEL_BG_COLOR);
+        mRecentPanelBgColor.setOnPreferenceChangeListener(this);
+        mRecentPanelBgColor.setSummary(mRecentPanelBgColor.getSummaryText() + ColorPickerPreference.convertToARGB(Settings.System.getInt(resolver,
+                     Settings.System.RECENT_PANEL_BG_COLOR, mRecentPanelBgColor.getPrefDefault())));
+        mRecentPanelBgColor.setNewPreviewColor(Settings.System.getInt(resolver, Settings.System.RECENT_PANEL_BG_COLOR, mRecentPanelBgColor.getPrefDefault()));
+
+        mRecentCardBgColor = (ColorPickerPreference) findPreference(RECENT_CARD_BG_COLOR);
+        mRecentCardBgColor.setOnPreferenceChangeListener(this);
+        mRecentCardBgColor.setSummary(mRecentCardBgColor.getSummaryText() + ColorPickerPreference.convertToARGB(Settings.System.getInt(resolver,
+                     Settings.System.RECENT_CARD_BG_COLOR, mRecentCardBgColor.getPrefDefault())));
+        mRecentCardBgColor.setNewPreviewColor(Settings.System.getInt(resolver, Settings.System.RECENT_CARD_BG_COLOR, mRecentCardBgColor.getPrefDefault()));
+
+        mRecentCardTextColor = (ColorPickerPreference) findPreference(RECENT_CARD_TEXT_COLOR);
+        mRecentCardTextColor.setOnPreferenceChangeListener(this);
+        mRecentCardTextColor.setSummary(mRecentCardTextColor.getSummaryText() + ColorPickerPreference.convertToARGB(Settings.System.getInt(resolver,
+                     Settings.System.RECENT_CARD_TEXT_COLOR, mRecentCardTextColor.getPrefDefault())));
+        mRecentCardTextColor.setNewPreviewColor(Settings.System.getInt(resolver, Settings.System.RECENT_CARD_TEXT_COLOR, mRecentCardTextColor.getPrefDefault()));
+
+        updatePreference();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        updatePreference();
+    }
+
+    private void updatePreference() {
+        boolean slimRecent = Settings.System.getInt(getActivity().getContentResolver(),
+                    Settings.System.USE_SLIM_RECENTS, 0) == 1;
+
+        if (slimRecent) {
+            mRecentsSearchBar.setEnabled(false);
+            mRecentsClearAll.setEnabled(false);
+            mRecentsClearAllLocation.setEnabled(false);
+            mRecentsDismissAll.setEnabled(false);
+            mRecentPanelBgColor.setPreviewDim(true);
+            mRecentCardBgColor.setPreviewDim(true);
+            mRecentCardTextColor.setPreviewDim(true);
+        } else {
+            mRecentsSearchBar.setEnabled(true);
+            mRecentsClearAll.setEnabled(true);
+            mRecentsClearAllLocation.setEnabled(true);
+            mRecentsDismissAll.setEnabled(true);
+            mRecentPanelBgColor.setPreviewDim(false);
+            mRecentCardBgColor.setPreviewDim(false);
+            mRecentCardTextColor.setPreviewDim(false);
+        }
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         ContentResolver resolver = getActivity().getContentResolver();
-        if (preference == mRecentsClearAll) {
-            boolean show = (Boolean) newValue;
-            Settings.System.putIntForUser(getActivity().getContentResolver(),
-                    Settings.System.SHOW_CLEAR_ALL_RECENTS, show ? 1 : 0, UserHandle.USER_CURRENT);
-            return true;
-        } else if (preference == mRecentsClearAllLocation) {
+        if (preference == mRecentsClearAllLocation) {
             int location = Integer.valueOf((String) newValue);
+            int index = mRecentsClearAllLocation.findIndexOfValue((String) newValue);
             Settings.System.putIntForUser(getActivity().getContentResolver(),
                     Settings.System.RECENTS_CLEAR_ALL_LOCATION, location, UserHandle.USER_CURRENT);
-            updateRecentsLocation(location);
+            mRecentsClearAllLocation.setSummary(mRecentsClearAllLocation.getEntries()[index]);
+            return true;
+        } else if (preference == mUseSlimRecents) {
+            Settings.System.putInt(getContentResolver(), Settings.System.USE_SLIM_RECENTS,
+                    ((Boolean) newValue) ? 1 : 0);
+            updatePreference();
+            return true;
+        } else if (preference == mMaxApps) {
+            int maxApps = (Integer) newValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.RECENTS_MAX_APPS, maxApps);
+            return true;
+        } else if (preference == mRecentPanelScale) {
+            int recentPanelScale = (Integer) newValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.RECENT_PANEL_SCALE_FACTOR, recentPanelScale);
+            return true;
+        } else if (preference == mRecentPanelExpandedMode) {
+            int recentPanelExpandedMode = Integer.valueOf((String) newValue);
+            int index = mRecentPanelExpandedMode.findIndexOfValue((String) newValue);
+            Settings.System.putIntForUser(getActivity().getContentResolver(),
+                    Settings.System.RECENT_PANEL_EXPANDED_MODE,
+                recentPanelExpandedMode, UserHandle.USER_CURRENT);
+            mRecentPanelExpandedMode.setSummary(mRecentPanelExpandedMode.getEntries()[index]);
+            return true;
+        } else if (preference == mRecentPanelLeftyMode) {
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.RECENT_PANEL_GRAVITY,
+                    ((Boolean) newValue) ? Gravity.LEFT : Gravity.RIGHT);
+            return true;
+        } else if (preference == mRecentPanelBgColor) {
+            Settings.System.putInt(getActivity().getContentResolver(), Settings.System.RECENT_PANEL_BG_COLOR, (Integer) newValue);
+            preference.setSummary(((ColorPickerPreference) preference).getSummaryText() + ColorPickerPreference.convertToARGB((Integer) newValue));
+            return true;
+        } else if (preference == mRecentCardBgColor) {
+            Settings.System.putInt(getActivity().getContentResolver(), Settings.System.RECENT_CARD_BG_COLOR, (Integer) newValue);
+            preference.setSummary(((ColorPickerPreference) preference).getSummaryText() + ColorPickerPreference.convertToARGB((Integer) newValue));
+            return true;
+        } else if (preference == mRecentCardTextColor) {
+            Settings.System.putInt(getActivity().getContentResolver(), Settings.System.RECENT_CARD_TEXT_COLOR, (Integer) newValue);
+            preference.setSummary(((ColorPickerPreference) preference).getSummaryText() + ColorPickerPreference.convertToARGB((Integer) newValue));
             return true;
         }
         return false;
-    }
-
-    private void updateRecentsLocation(int value) {
-        ContentResolver resolver = getContentResolver();
-        Resources res = getResources();
-        int summary = -1;
-
-        Settings.System.putInt(resolver, Settings.System.RECENTS_CLEAR_ALL_LOCATION, value);
-
-        if (value == 0) {
-            Settings.System.putInt(resolver, Settings.System.RECENTS_CLEAR_ALL_LOCATION, 0);
-            summary = R.string.recents_clear_all_location_top_right;
-        } else if (value == 1) {
-            Settings.System.putInt(resolver, Settings.System.RECENTS_CLEAR_ALL_LOCATION, 1);
-            summary = R.string.recents_clear_all_location_top_left;
-        } else if (value == 2) {
-            Settings.System.putInt(resolver, Settings.System.RECENTS_CLEAR_ALL_LOCATION, 2);
-            summary = R.string.recents_clear_all_location_top_center;
-        } else if (value == 3) {
-            Settings.System.putInt(resolver, Settings.System.RECENTS_CLEAR_ALL_LOCATION, 3);
-           summary = R.string.recents_clear_all_location_bottom_right;
-        } else if (value == 4) {
-            Settings.System.putInt(resolver, Settings.System.RECENTS_CLEAR_ALL_LOCATION, 4);
-            summary = R.string.recents_clear_all_location_bottom_left;
-        } else if (value == 5) {
-            Settings.System.putInt(resolver, Settings.System.RECENTS_CLEAR_ALL_LOCATION, 5);
-            summary = R.string.recents_clear_all_location_bottom_center;
-        }
-        if (mRecentsClearAllLocation != null && summary != -1) {
-            mRecentsClearAllLocation.setSummary(res.getString(summary));
-        }
     }
 }
