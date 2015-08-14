4b11d44cde8ec95ccec3d5cd21077e63e0b486c6
Muhammed Nazim <mnm9994u@gmail.com>
2015-08-14 16:06:22 -0400

    Forward Port: Add Camera sound toggle [2/3]
    PureNexus edits:
    * killed the dialog popup i come from touchwiz where we kill all the popups lol

--- src/com/android/settings/nexus/VolNotifSettings.java
+++ src/com/android/settings/nexus/VolNotifSettings.java
@@ -18,18 +18,24 @@ package com.android.settings.nexus;
 
 import android.content.ContentResolver;
 import android.os.Bundle;
+import android.os.SystemProperties;
 import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
 import android.provider.Settings;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
 
-public class VolNotifSettings extends SettingsPreferenceFragment {
+public class VolNotifSettings extends SettingsPreferenceFragment implements
+         OnPreferenceChangeListener {
 
     private static final String KEY_HEADS_UP_SETTINGS = "heads_up_enabled";
+    private static final String KEY_CAMERA_SOUNDS = "camera_sounds";
+    private static final String PROP_CAMERA_SOUND = "persist.sys.camera-sound";
 
     private Preference mHeadsUp;
+    private SwitchPreference mCameraSounds;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -37,6 +43,21 @@ public class VolNotifSettings extends SettingsPreferenceFragment {
         addPreferencesFromResource(R.xml.nexus_notif_settings);
 		
         mHeadsUp = findPreference(KEY_HEADS_UP_SETTINGS);
+        mCameraSounds = (SwitchPreference) findPreference(KEY_CAMERA_SOUNDS);
+        mCameraSounds.setChecked(SystemProperties.getBoolean(PROP_CAMERA_SOUND, true));
+        mCameraSounds.setOnPreferenceChangeListener(this);
+    }
+
+     public boolean onPreferenceChange(Preference preference, Object objValue) {
+        final String key = preference.getKey();
+        if (KEY_CAMERA_SOUNDS.equals(key)) {
+           if ((Boolean) objValue) {
+               SystemProperties.set(PROP_CAMERA_SOUND, "1");
+           } else {
+               SystemProperties.set(PROP_CAMERA_SOUND, "0");
+           }
+        }
+        return true;
     }
 
     private boolean getUserHeadsUpState() {
