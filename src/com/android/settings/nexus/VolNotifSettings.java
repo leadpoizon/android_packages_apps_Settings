d5acbd58ace3abb4f15752525c50c2e04aa16e4e
Lars Greiss <kufikugel@googlemail.com>
2015-08-14 18:56:00 -0400

    Settings: less notifications sound (2/2) Add back the 4.3 feature like it was.
    User can setup how often notification sound
    for an app comes trough so that
    it does not annoy to much
    
    Also add fix less frequent notifications summary
    Credits: Altaf-Mahdi

--- src/com/android/settings/nexus/VolNotifSettings.java
+++ src/com/android/settings/nexus/VolNotifSettings.java
@@ -19,8 +19,11 @@ package com.android.settings.nexus;
 import android.content.ContentResolver;
 import android.os.Bundle;
 import android.os.SystemProperties;
-import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
+import android.preference.SwitchPreference;
 import android.provider.Settings;
 
 import com.android.settings.R;
@@ -30,25 +33,35 @@ import com.android.settings.Utils;
 public class VolNotifSettings extends SettingsPreferenceFragment implements
          OnPreferenceChangeListener {
 
+    private static final String PREF_LESS_NOTIFICATION_SOUNDS = "less_notification_sounds";
     private static final String KEY_HEADS_UP_SETTINGS = "heads_up_enabled";
     private static final String KEY_CAMERA_SOUNDS = "camera_sounds";
     private static final String PROP_CAMERA_SOUND = "persist.sys.camera-sound";
 
     private Preference mHeadsUp;
+    private ListPreference mAnnoyingNotifications;
     private SwitchPreference mCameraSounds;
 
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         addPreferencesFromResource(R.xml.nexus_notif_settings);
-		
+
+        final PreferenceScreen prefScreen = getPreferenceScreen();
+
         mHeadsUp = findPreference(KEY_HEADS_UP_SETTINGS);
+
+        mAnnoyingNotifications = (ListPreference) findPreference(PREF_LESS_NOTIFICATION_SOUNDS);
+        int notificationThreshold = Settings.System.getInt(getContentResolver(),
+                Settings.System.MUTE_ANNOYING_NOTIFICATIONS_THRESHOLD, 0);
+        updateAnnoyingNotificationValues();
+
         mCameraSounds = (SwitchPreference) findPreference(KEY_CAMERA_SOUNDS);
         mCameraSounds.setChecked(SystemProperties.getBoolean(PROP_CAMERA_SOUND, true));
         mCameraSounds.setOnPreferenceChangeListener(this);
     }
 
-     public boolean onPreferenceChange(Preference preference, Object objValue) {
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
         final String key = preference.getKey();
         if (KEY_CAMERA_SOUNDS.equals(key)) {
            if ((Boolean) objValue) {
@@ -57,7 +70,25 @@ public class VolNotifSettings extends SettingsPreferenceFragment implements
                SystemProperties.set(PROP_CAMERA_SOUND, "0");
            }
         }
+        if (PREF_LESS_NOTIFICATION_SOUNDS.equals(key)) {
+            final int val = Integer.valueOf((String) objValue);
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.MUTE_ANNOYING_NOTIFICATIONS_THRESHOLD, val);
+            updateAnnoyingNotificationValues();
+        }
         return true;
+    }
+
+    private void updateAnnoyingNotificationValues() {
+        int notificationThreshold = Settings.System.getInt(getContentResolver(),
+                Settings.System.MUTE_ANNOYING_NOTIFICATIONS_THRESHOLD, 0);
+        if (mAnnoyingNotifications == null) {
+            mAnnoyingNotifications.setSummary(getString(R.string.less_notification_sounds_summary));
+        } else {
+            mAnnoyingNotifications.setValue(Integer.toString(notificationThreshold));
+            mAnnoyingNotifications.setSummary(mAnnoyingNotifications.getEntry());
+            mAnnoyingNotifications.setOnPreferenceChangeListener(this);
+        }
     }
 
     private boolean getUserHeadsUpState() {
@@ -73,5 +104,4 @@ public class VolNotifSettings extends SettingsPreferenceFragment implements
         mHeadsUp.setSummary(getUserHeadsUpState()
                 ? R.string.summary_heads_up_enabled : R.string.summary_heads_up_disabled);
     }
-
 }
